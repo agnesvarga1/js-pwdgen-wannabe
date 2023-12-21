e0555eddc004993452be9a66a60a3be49c81aee4
agnesvarga1 <agikolyok@gmail.com>
2023-12-21 15:09:17 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+let nome = prompt(" Quale e la il tuo nome?");
+document.querySelector("#name").innerHTML = "Nome: " + nome;
+let cNome = prompt("Quale e il tuo cognome?");
+document.querySelector("#lname").innerHTML = "Cognome: " + cNome;
+let favColor = prompt("Quale il tuo colore prferito?");
+document.querySelector("#color").innerHTML = "Colore preferito: " + favColor;
