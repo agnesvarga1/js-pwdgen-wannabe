1542b1bb19b0ff7f3e862049a99862f90ab1c59c
agnesvarga1 <agikolyok@gmail.com>
2023-12-21 16:29:39 +0100

    pwd

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const nome = prompt(" Quale e la il tuo nome?");
-document.querySelector("#name").innerHTML = "Nome: " + nome;
 const cNome = prompt("Quale e il tuo cognome?");
-document.querySelector("#lname").innerHTML = "Cognome: " + cNome;
 const favColor = prompt("Quale il tuo colore prferito?");
+document.querySelector("#name").innerHTML = "Nome: " + nome;
+document.querySelector("#lname").innerHTML = "Cognome: " + cNome;
 document.querySelector("#color").innerHTML = "Colore preferito: " + favColor;
+document.querySelector("h3").innerHTML =
+  "pwd: " + nome + cNome + favColor + "23";
